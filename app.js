61de263e46532e2b94d289a91c150bd0d743a06c
BONGJIN CHO <bjchomr@naver.com>
2022-12-30 21:10:33 +0900

    수업도중 백업

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
 
 const app = express();
 const port = 3000;
 
+app.use(cookieParser())
+app.use(express.json())
+
+const users = [
+    {id: "qwerty"},
+    {id: "asdfgh"},
+    {id: "zxcvbn"}
+]
+
 app.get('/users', (req, res) => {
-    res.send('users 페이지 입니다')
+    const userId = req.cookies.userid
+    const user = users.find(user => user.id === userId)
+    // console.log(userId)
+    res.send(user)
 })
 
 app.post('/login', (req, res) => {
