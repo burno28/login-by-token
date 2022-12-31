aeb3a14e14bc212dc15116b634de9e9b3993a0a0
BONGJIN CHO <bjchomr@naver.com>
2022-12-31 19:38:53 +0900

    늦은숙제

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const MemoryStore = require('memorystore')(session)
 
 const app = express();
 const port = 3000;
@@ -7,21 +9,52 @@ const port = 3000;
 app.use(cookieParser())
 app.use(express.json())
 
+// const users = [
+//     {id: "qwerty"},
+//     {id: "asdfgh"},
+//     {id: "zxcvbn"}
+// ]
+
+//저장소
+// const sessions = []
+
+const sessionObj = {
+    secret: 'node',
+    resave: false,
+    saveUninitialized: true,
+    store: new MemoryStore(),
+};
+app.use(session(sessionObj))
+
 const users = [
-    {id: "qwerty"},
-    {id: "asdfgh"},
-    {id: "zxcvbn"}
+    { id: "bongjin" },
+    { id: "jinbong" },
+    { id: "chobong" }
 ]
 
 app.get('/users', (req, res) => {
-    const userId = req.cookies.userid
+    const userId = req.session.userid
+    //     const userId = req.cookies.userid
     const user = users.find(user => user.id === userId)
-    // console.log(userId)
+    //     const ssid = "asdfasdfasdfadf"
+    //     sessions.push({
+    //         ...user,
+    //         ssid
+    //     })
+    //     res.cookie("ssid", ssid)
+    //     console.log(session)
+    //     // console.log(userId)
     res.send(user)
+    // 
 })
 
 app.post('/login', (req, res) => {
-    res.send('login 페이지 입니다')
+    const userId = req.body.userid
+    const user = users.find(user => user.id === userId)
+
+    req.session.userid = user.id
+    res.send(user.id)
+    // res.send('login 페이지 입니다')
 })
 
 app.post('/logout', (req, res) => {
